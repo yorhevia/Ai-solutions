39a0c03267d68913274f5a86c0d7d4f20dbceca4
Alealaes04 <alealaes04@gmail.com>
2025-05-12 12:55:56 -0400

    Add views

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
+
+//OBTENER RUTA PRINCIPAL (LOGIN)
+//OBTENER RUTA PRINCIPAL (LOGIN)
+
+
 router.get('/', (req, res) =>{
-  res.render('login')
+  res.render('ingreso/login')
 })
+
+
+//OBTENER RUTA REGISTRO
+//OBTENER RUTA REGISTRO
 
 router.get('/registro', (req, res) =>{
-  res.render('registro')
+  res.render('ingreso/registro')
 })
 
+
+//OBTENER RUTA CONSULTA
+//OBTENER RUTA CONSULTA
+
 router.get('/consulta', (req, res) =>{
-  res.render('consulta')
+  res.render('asesor/consulta')
 })
+
+
+//OBTENER RUTA CONSULTA CLIENTE
+//OBTENER RUTA CONSULTA CLIENTE
+
+router.get('/consultacliente', (req, res)=> {
+  res.render('cliente/consultacliente')
+})
+
+
+//OBTENER RUTA PERFIL CLIENTE
+//OBTENER RUTA PERFIL CLIENTE
+router.get('/perfilcliente', (req, res) =>{
+  res.render('asesor/perfilcliente')
+})
+
+
+//OBTENER RUTA FORMULARIO CLIENTE
+//OBTENER RUTA FORMULARIO CLIENTE
+
+router.get('/formulariocliente', (req, res) =>{
+  res.render('cliente/formulariocliente')
+})
+
 
 module.exports = router;
