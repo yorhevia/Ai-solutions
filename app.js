fe8817fd77e78af069d51b532627bf4f858bb05a
Alealaes04 <alealaes04@gmail.com>
2025-05-12 17:57:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var createError = require('http-errors');
+const session = require('express-session');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
