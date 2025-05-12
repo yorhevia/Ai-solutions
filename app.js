4eb92e3b6fcdad1021062b1293fc206448f5678f
Alealaes04 <alealaes04@gmail.com>
2025-05-12 18:03:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
